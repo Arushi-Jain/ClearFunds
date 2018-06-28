13166dd419b57524382cc42263cc759f2b5d3db9
Arushi <arushijain016@gmail.com>
2018-06-28 22:42:07 +0530

    Responsive Chat Ui

--- selectbox/src/pages/index.vue
+++ selectbox/src/pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="row">
-    <div class="col-sm-4"></div>
+    <div class="col-sm-4">{{this.selected}}</div>
     <div class="col-sm-4">
     <q-select
       v-model="select"
       inverted-light
       color="grey-4"
-      filter="true"
-      autofocus-filter="true"
-      filter-placeholder= "Search"
+      filter
+      autofocus-filter
+      filter-placeholder= "Search..."
       separator
-      display-value="Select..."
+      :display-value="displayvalue()"
       static-label="Is Quasar Awesome?"
      :options="selectOptions"
     ></q-select>
@@ -26,7 +26,21 @@ export default {
   name: 'PageIndex',
   data () {
     return {
+      selected: [],
+      select: '',
       selectOptions: [ { label: 'Option One', value: 'one' }, { label: 'Option Two', value: 'two' }, { label: 'Option Three', value: 'three' } ]
+    }
+  },
+  watch: {
+    select () {
+      this.selected.push(this.select)
+    }
+  },
+  methods: {
+    displayvalue () {
+      if (this.selected.length === 0) {
+        return 'Select...'
+      }
     }
   }
 }
